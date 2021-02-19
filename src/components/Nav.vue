e4c36c5ab102b0cc7cefa4dae42327796d2cfda5
Sirius <83930223@qq.com>
2021-02-19 12:39:25 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money" >
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money"/>
       记账
     </router-link>
-    |
-    <router-link to="/Labels" >
+    <router-link to="/Labels" class="item">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/Statistics" >
+    <router-link to="/Statistics" class="item">
       <Icon name="statistic"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -26,5 +24,23 @@ name: "Nav"
 </script>
 
 <style lang="scss" scoped>
-.nav{border: 1px solid #ff0000;}
+nav{
+display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  flex-direction: row;
+  font-size: 12px;
+> .item {
+  padding: 2px 0;
+  width:33.33333%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .icon{
+    width: 32px;
+    height: 32px;
+  }
+
+  }
+}
 </style>
