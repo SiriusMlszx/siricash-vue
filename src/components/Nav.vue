52a0a501728a38045a507146fe04c3778a790380
Sirius <83930223@qq.com>
2021-02-19 11:42:44 +0800

    nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="nav">
-    <router-link to="/money" >记账</router-link>
+    <router-link to="/money" >记账
+    <svg>
+      <use xlink:href="#label"/>
+    </svg>
+    </router-link>
     |
     <router-link to="/Labels" >标签</router-link>
     |
@@ -9,11 +13,14 @@
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/label.svg';
+
+console.log(x);
 export default {
 name: "Nav"
 }
 </script>
 
 <style lang="scss" scoped>
-.nav{border: 1px solid red;}
+.nav{border: 1px solid #ff0000;}
 </style>
