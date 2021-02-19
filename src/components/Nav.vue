0c4032566ff78a1f4c0f657adada9e96fb2ed59b
Sirius <83930223@qq.com>
2021-02-19 12:26:18 +0800

    创建<Icon />

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="nav">
-    <router-link to="/money" >记账
-    <svg>
-      <use xlink:href="#label"/>
-    </svg>
+    <router-link to="/money" >
+      <Icon name="money"/>
+      记账
     </router-link>
     |
-    <router-link to="/Labels" >标签</router-link>
+    <router-link to="/Labels" >
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/Statistics" >统计</router-link>
+    <router-link to="/Statistics" >
+      <Icon name="statistic"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg';
 
-console.log(x);
+
 export default {
 name: "Nav"
 }
