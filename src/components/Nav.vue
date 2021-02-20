f2d8f98a91ae55cf9d00de0ff826fac555c07076
Sirius <83930223@qq.com>
2021-02-20 10:31:57 +0800

    HTML和CSS写好了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -26,8 +26,8 @@ name: "Nav"
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 nav{
-display: flex;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  @extend %outerShadow;
+  display: flex;
   flex-direction: row;
   font-size: 12px;
 > .item {
