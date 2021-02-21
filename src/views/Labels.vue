490b3f3c4c3f545d7a5d9faca8919d64ad47da7f
Sirius <83930223@qq.com>
2021-02-21 16:11:20 +0800

    完成编辑标签页面的CSS，提取了按钮通用组件

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -5,33 +5,37 @@
                    v-for="tag in tags"
                    :key="tag.id"
                    :to="`/labels/edit/${tag.id}`">
-        <span>{{tag.name}}</span>
+        <span>{{ tag.name }}</span>
         <Icon name="right"></Icon>
       </router-link>
     </div>
     <div class="createTag-wrapper">
-      <button class="createTag" @click="createTag">新建标签</button>
+      <Button class="createTag" @click="createTag">新建标签</Button>
     </div>
   </Layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 import Component from "vue-class-component";
 import tagListModel from "@/models/tagListModel";
+import Button from "@/components/Button.vue";
 
-tagListModel.fetch()
-@Component
-export default class Labels extends Vue{
-  tags = tagListModel.data
-  createTag(){
-    const name = window.prompt('请输入标签名')
-    if(name){
-        const message = tagListModel.create(name);
-        if(message === 'duplicated'){
-          window.alert('标签名重复啦')
-        }else if(message === 'success'){
-          window.alert('添加成功')
+tagListModel.fetch();
+@Component({
+  components: {Button}
+})
+export default class Labels extends Vue {
+  tags = tagListModel.data;
+
+  createTag() {
+    const name = window.prompt("请输入标签名");
+    if (name) {
+      const message = tagListModel.create(name);
+      if (message === "duplicated") {
+        window.alert("标签名重复啦");
+      } else if (message === "success") {
+        window.alert("添加成功");
       }
     }
   }
@@ -43,13 +47,15 @@ export default class Labels extends Vue{
   background: white;
   font-size: 16px;
   padding-left: 16px;
+
   > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
     justify-content: space-between;
     border-bottom: 1px solid #e6e6e6;
-    svg{
+
+    svg {
       width: 18px;
       height: 18px;
       color: #666;
@@ -57,19 +63,20 @@ export default class Labels extends Vue{
     }
   }
 }
-.createTag{
+
+.createTag {
   background: #767676;
   color: white;
   border-radius: 4px;
   border: none;
   height: 40px;
   padding: 0 16px;
-  &-wrapper{
-  text-align: center;
+
+  &-wrapper {
+    text-align: center;
     padding: 16px 0;
-    margin-top: 44-16px
-  ;
-}
+    margin-top: 44-16px;
+  }
 
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,9 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -12,7 +14,7 @@
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
-import Notes from "@/components/Money/Notes.vue";
+import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import Component from "vue-class-component";
 import {Watch} from "vue-property-decorator";
@@ -23,7 +25,7 @@ const recordList = recordListModel.fetch();
 const tagList = tagListModel.fetch()
 
 @Component({
-  components: {Tags, Notes, Types, NumberPad},
+  components: {Tags, FormItem, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = tagList;
@@ -54,6 +56,9 @@ export default class Money extends Vue {
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
+}
+.notes{
+  padding: 12px 0;
 }
 </style>
 <style lang="scss" scoped>
