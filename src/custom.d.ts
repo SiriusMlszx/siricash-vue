5cb3cc02869bc507e0bcb755d2bb0e0d26ee47cc
Sirius <83930223@qq.com>
2021-02-22 11:47:26 +0800

    将所有数据操作封装在store里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -21,11 +21,13 @@ type TagListModel = {
 }
 
 interface Window {
-    tagList: Tag[];
-    createTag: (name: string) => void;
-    removeTag: (id: string) => boolean;
-    updateTag: TagListModel["update"];
-    findTag: (id: string) => Tag | undefined;
-    recordList: RecordItem[];
-    createRecord: (record: RecordItem) => void;
+    store: {
+        tagList: Tag[];
+        createTag: (name: string) => void;
+        removeTag: (id: string) => boolean;
+        updateTag: TagListModel["update"];
+        findTag: (id: string) => Tag | undefined;
+        recordList: RecordItem[];
+        createRecord: (record: RecordItem) => void;
+    };
 }
