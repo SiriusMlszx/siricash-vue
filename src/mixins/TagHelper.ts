6a6f2db2926642bbc04e309b0ffd3abd89432013
Sirius <83930223@qq.com>
2021-02-25 18:31:45 +0800

    添加时间输入

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -5,7 +5,8 @@ import Vue from "vue";
 export class TagHelper extends Vue {
     createTag() {
         const name = window.prompt("请输入标签名");
-        if (!name) {return window.alert("标签名不能为空");}
+        if (name==='') {return window.alert("标签名不能为空");}
+        else if(name===null){return}
         this.$store.commit("createTag", name);
     }
 }
