af9947a3dea83d2bd92a963647faf40b37ff4e7d
Sirius <83930223@qq.com>
2021-02-25 13:06:18 +0800

    添加二维码

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,15 @@ window.onload = function (){
         window.scrollTo(0, 200)
     }, 0)
 }
+
+if(document.documentElement.clientWidth > 500){
+    window.alert('用手机浏览效果会更好哦！')
+    const img = document.createElement('img')
+    img.src = '/QRCode.png'
+    img.style.position = 'fixed'
+    img.style.left = '50%'
+    img.style.top = '50%'
+    img.style.transform = 'translate(-50%, -50%)'
+    img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+    document.body.appendChild(img)
+}
